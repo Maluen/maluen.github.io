b55535d63e965be135a80ee409ed7e710689ba0d
Manuel Dell'Elce <mh025r0x@gmail.com>
2018-08-25 11:25:15 +0200

    Update with some personal content

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -26,7 +26,7 @@ var cbpAnimatedHeader = (function() {
 
 	function scrollPage() {
 		var sy = scrollY();
-		if ( sy >= changeHeaderOn ) {
+		if ( sy >= window.innerHeight ) {
 			classie.add( header, 'navbar-shrink' );
 		}
 		else {
